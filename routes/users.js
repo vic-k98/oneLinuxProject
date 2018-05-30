217962cc536102be10dc23f544901d6d98424cd9
vic <vic98k27149@gmail.com>
2018-05-30 22:11:35 +0800

    添加页面小图标

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('users/users', {title: 'User Center'});
+    res.render('users/users', {title: 'User Center', iconLink: '//img.alicdn.com/tps/TB1HwXxLpXXXXchapXXXXXXXXXX-32-32.ico'});
 });
 
 module.exports = router;
