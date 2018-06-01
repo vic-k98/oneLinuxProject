636e77cd5bb2145f97affc08b81f5ff319a3d318
guoxiu <guoxiu@globalegrow.com>
2018-06-01 14:05:51 +0800

    调试分享

--- public/src/js/users/component/common/Header.vue
+++ public/src/js/users/component/common/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="header">
         <!-- 折叠按钮 -->
-        <div class="collapseBtn">
+        <div class="collapseBtn js-share">
             <i class="el-icon-menu"></i>
         </div>
         <div class="logo">后台管理系统</div>
@@ -14,6 +14,36 @@
             return {
 
             }
+        },
+        methods: {
+            share() {
+                const fjs = document.getElementsByTagName('script')[0];
+                if (document.getElementById('pinterest-jssdk')) return;
+                const js = document.createElement('script');
+                js.id = 'pinterest-jssdk';
+                js.src = '//assets.pinterest.com/js/pinit.js';
+                fjs.parentNode.insertBefore(js, fjs);
+                js.onload = () => {
+                    console.log(document.getElementsByClassName('js-share'));
+                    document.getElementsByClassName('js-share')[0].onclick = function() {
+                        PinUtils.pinAny();
+                    }
+                    // $(document).on('click', '.js-share', () => {
+                    //     onClick();
+                    //     if (isLogin) {
+                    //         const curIsLogin = await userIsLogin();
+                    //         if (!curIsLogin) {
+                    //             window.location.href = Share.loginUrl;
+                    //             return;
+                    //         }
+                    //     }
+                    //     PinUtils.pinAny() // eslint-disable-line
+                    // });
+                };
+            }
+        },
+        created() {
+            this.share();
         }
     }
 </script>
