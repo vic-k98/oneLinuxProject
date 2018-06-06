5a914d7863474acc556c45aa721fb6560fb777de
guoxiu <guoxiu@globalegrow.com>
2018-06-06 19:18:57 +0800

    布局登陆页面

--- public/src/js/users/component/page/Login.vue
+++ public/src/js/users/component/page/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="login-wrap">
         <div class="login-content">
-            
+
         </div>
     </div>
 </template>
@@ -14,9 +14,20 @@
 
 <style lang="less">
     .login-wrap {
+        position: relative;
         width: 100%;
         height: 100%;
         background: url('../../../../bgImg/usersBg.jpg') no-repeat center;
         background-size: cover;
+    }
+    .login-content {
+        position: absolute;
+        left: 50%;
+        top: 40%;
+        margin-left: -220px; 
+        width: 360px;
+        padding: 40px;
+        border-radius: 5px;
+        background: rgba(255,255,255,.8);
     }
 </style>
